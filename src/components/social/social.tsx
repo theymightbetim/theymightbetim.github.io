2894fded63325b35788696ddb5aa497818e74426
Tim Mackenzie <theymightbetim@gmail.com>
2025-03-15 10:38:39 -0400

    export SocialLinks

---
+++ src/components/social/social.tsx
@@ -0,0 +1,12 @@
+export const SocialLinks = () => {
+    return (
+    <div className="SocialLinks">
+        <a href="https://linkedin.com/in/timothy-mackenzie">
+        <FontAwesomeIcon icon={faLinkedin} /> LinkedIn
+        </a>  |  
+        <a href="https://github.com/theymightbetim">
+            <FontAwesomeIcon icon={faGithub} /> GitHub
+        </a>
+    </div>
+    )
+}
