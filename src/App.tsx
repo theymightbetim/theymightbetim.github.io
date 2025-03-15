fdff48e5c9a03c3e0e11bbae073804d849e4334a
Tim Mackenzie <theymightbetim@gmail.com>
2025-03-15 09:41:02 -0400

    under construction

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,17 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <Header />
+      <Intro />
     </>
   )
 }
 
-const header = () => {
+const Header = () => {
   return <h1>Tim Mackenzie - Quality Assurance Professional</h1>
 }
 
-const intro = () => {
+const Intro = () => {
   return <p>Welcome to my website! It's under construction, but check back soon!</p>
 }
 
