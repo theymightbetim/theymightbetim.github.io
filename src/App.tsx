44c20ce5321ea70bb7b44ee98a693a3c8dd6ad93
Tim Mackenzie <theymightbetim@gmail.com>
2025-03-15 09:52:35 -0400

    add social media

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 function App() {
   return (
     <>
       <Header />
       <Intro />
+      <SocialLinks />
     </>
   )
 }
@@ -15,6 +18,19 @@ const Header = () => {
 
 const Intro = () => {
   return <p>Welcome to my website! It's under construction, but check back soon!</p>
+}
+
+const SocialLinks = () => {
+  return (
+    <div>
+      <a href="https://linkin.com/in/timothy-mackenzie">
+        <FontAwesomeIcon icon={faLinkedin} /> LinkedIn
+      </a>
+      <a href="https://github.com/theymightbetim">
+        <FontAwesomeIcon icon={faGithub} /> GitHub
+      </a>
+    </div>
+    )
 }
 
 export default App
