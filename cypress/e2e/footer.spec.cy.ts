6dc513e302615c41c4a3209c5d2b50566802bdd7
Tim Mackenzie <theymightbetim@gmail.com>
2025-03-18 10:17:58 -0400

    add footer tests

---
+++ cypress/e2e/footer.spec.cy.ts
@@ -0,0 +1,11 @@
+describe('Footer Tests', () => {
+    beforeEach(() => {
+        cy.visit('https://theymightbetim.github.io/')
+    })
+    it('should display the footer', () => {
+        cy.get('.footer')
+            .should('be.exist')
+            .should('be.visible')
+            .should('contain.text', 'Built with')
+    })
+})
