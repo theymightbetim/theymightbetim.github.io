d2d0d0af23c347a2283acfef1369c111f2dc72ab
Tim Mackenzie <theymightbetim@gmail.com>
2025-03-18 10:26:45 -0400

    update readme

--- src/components/nav/navbar.tsx
+++ src/components/nav/navbar.tsx
@@ -9,16 +9,20 @@ export const Navbar = () => {
                 <a href="/" data-testid="home-link">Home</a>
             </div>
             <div className="navlink">
-                <a href="/about" data-testid="about-link">About</a>
+                <a href="/about" className="disabled" data-testid="about-link">About</a>
             </div>
             <div className="navlink">
-                <a href="https://github.com/theymightbetim?tab=repositories" data-testid="projects-link"> <FontAwesomeIcon icon={faGithub} />Projects</a>
+                <a href="https://github.com/theymightbetim?tab=repositories"
+                    data-testid="projects-link"> <FontAwesomeIcon icon={faGithub} />Projects</a>
             </div>
             <div className="navlink">
-                <a href="/blog" data-testid="blog-link">Blog</a>
+                <a href="/blog"
+                    className="disabled"
+                    data-testid="blog-link">Blog</a>
             </div>
             <div className="navlink">
-                <a href="/contact" data-testid="contact-link">Contact</a>
+                <a href="/contact"
+                className='disabled' data-testid="contact-link">Contact</a>
             </div>
         </div>
     )
